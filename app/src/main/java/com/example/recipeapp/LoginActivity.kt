a9d2af5a50b6c34c7fb76ffa67b1ca3690a19d7f
James-Havinga <180099@virtualwindow.co.za>
2020-08-20 11:27:07 +0200

    fixed header

--- app/src/main/java/com/example/recipeapp/LoginActivity.kt
+++ app/src/main/java/com/example/recipeapp/LoginActivity.kt
@@ -13,6 +13,7 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        supportActionBar?.title = "Foodie"
 
         val auth = FirebaseAuth.getInstance()
         if (auth.currentUser !=null) {
